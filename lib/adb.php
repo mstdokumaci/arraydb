de6e841c46b464a12adc5ff85f3da57edf4ab72c
mustafa <mustafa@foraa.com>
2012-04-29 10:48:55 +0300

    wip

--- lib/adb.php
+++ lib/adb.php
@@ -13,30 +13,30 @@
 				return;
 			}
 
-			$this->DM=$DM+$this->get_initial_arrays();
+			$this->DM=$DM;
 
-			foreach ($DM as $name=>$array) {
-				$array=$array+$this->get_initial_fields();
-				$array['conf']=$array['conf']+$this->get_initial_config();
+			foreach ($DM as $name=>$item) {
+				$item=$item+$this->get_initial_item();
+				$item['conf']=$item['conf']+$this->get_initial_config();
 
-				foreach ($array['fields'] as $f_name=>$field) {
+				foreach ($item['fields'] as $f_name=>$field) {
 					$field=$field+$this->get_initial_field_data($field['type']);
-					$array['fields'][$f_name]=$field;
+					$item['fields'][$f_name]=$field;
 				}
 
-				foreach ($array['has_many'] as $local_name=>$has_many) {
+				foreach ($item['has_many'] as $local_name=>$has_many) {
 					$has_many['local_name']=is_string($local_name) ? $local_name : $has_many['type'];
 					if (!(isset($has_many['foreign_name']))) $has_many['foreign_name']=$name;
 					$this->DM[$has_many['type']]['fields'][$has_many['foreign_name']]=array(
 						'type'=>'numeric',
-						'len'=>$array['conf']['len'],
+						'len'=>$item['conf']['len'],
 						'foreign'=>array('type'=>$name, 'field'=>$has_many['local_name']),
 						'index'=>true
 					) + get_initial_field_data($field['type']);
-					$array['has_many'][$local_name]=$has_many;
+					$item['has_many'][$local_name]=$has_many;
 				}
 
-				foreach ($array['many_to_many'] as $local_name=>$m2m) {
+				foreach ($item['many_to_many'] as $local_name=>$m2m) {
 					if (isset($this->DM[$name]['many_to_many'][$local_name]['done'])) continue;
 					$m2m['local_name']=is_string($local_name) ? $local_name : $m2m['type'];
 					if (!isset($m2m['foreign_name'])) $m2m['foreign_name']=$name;
@@ -52,34 +52,69 @@
 						'foreign_name'=>$m2m['local_name'],
 						'relation_name'=>$m2m['relation_name']
 					);
-					$array['many_to_many'][$local_name]=$m2m;
+					$item['many_to_many'][$local_name]=$m2m;
 				}
 
-				$this->DM[$name]=$array;
+				$this->DM[$name]=$item;
 			}
 
 			cache::set('__DATA_MODEL_HASH__', md5(serialize($DM)));
 			cache::set('__DATA_MODEL__', $this->DM);
 		}
 
-		private function get_initial_arrays () {
-			static $list=array();
-			return $list;
+		function load ($name, $id) {
+			if (!isset($this->DM[$name])) throw new Exception('Undefined item name: ' . $name);
+
+			if (isset($this->ITEM[$name][$id])) return $this->ITEM[$name][$id];
+			elseif ($item=cache::get('item_' . $name . '_' . $id)) return $item;
+
+			$item=new ITEM($name, $id);
+			return $ITEM;
 		}
 
-		private function get_initial_fields () {
-			static $list=array();
+
+		private function get_initial_item () {
+			static $list=array(
+				'conf'=>array(),
+				'has_one'=>array(),
+				'has_many'=>array(),
+				'many_to_many'=>array(),
+				'self_ref'=>array(),
+				'fields'=>array()
+			);
 			return $list;
 		}
 
 		private function get_initial_config () {
-			static $list=array();
+			static $list=array(
+				'len'=>5,
+				'ttl'=>3600,
+			);
 			return $list;
 		}
 
 		private function get_initial_field_data ($type) {
-			static $list=array();
-			static $list_for_type=array();
+			static $list=array(
+				'type'=>'text',
+				'unique'=>false,
+				'index'=>false,
+				'foreign'=>false
+			);
+
+			static $list_for_type=array(
+				'text'=>array(
+					'len'=>100
+				),
+				'numeric'=>array(
+					'len'=>4,
+					'decimal'=>0,
+					'signed'=>false,
+				),
+				'pass'=>array(
+					'len'=>40,
+					'filter'=>'sha1'
+				),
+			);
 			return $list + $list_for_type[$type];
 		}
 	}
