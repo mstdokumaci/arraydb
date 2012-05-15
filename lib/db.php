260142896716d1f459b945f3e27a472308f50b3d
mustafa <mustafa@foraa.com>
2012-05-15 16:24:37 +0300

    bug fix

--- lib/db.php
+++ lib/db.php
@@ -21,19 +21,19 @@
 
 		function insert ($table, $data) {
 			foreach ($data as $k=>$v) $data[$k]=$k . "='" . $this->escape($v) . "'";
-			if (!mysqli_query($this->conn, "INSERT INTO " . $name . " SET " . implode(', ', $data)))
+			if (!mysqli_query($this->conn, "INSERT INTO " . $table . " SET " . implode(', ', $data)))
 				throw new Exception('MySQL insert query error: ' . mysqli_error($this->conn));
 			return mysqli_insert_id($this->conn);
 		}
 
 		function update ($table, $data, $condition='TRUE') {
 			$data=array_map(function ($k, $v) {return $k . "='" . $this->escape($v) . "'";}, $data);
-			if (!mysqli_query($this->conn, "UPDATE " . $name . " SET " . implode(', ', $data) . " WHERE " . $condition))
+			if (!mysqli_query($this->conn, "UPDATE " . $table . " SET " . implode(', ', $data) . " WHERE " . $condition))
 				throw new Exception('MySQL update query error: ' . mysqli_error($this->conn));
 		}
 
 		function delete ($table, $condition) {
-			if (!mysqli_query($this->conn, "DELETE FROM " . $name . " WHERE " . $condition))
+			if (!mysqli_query($this->conn, "DELETE FROM " . $table . " WHERE " . $condition))
 				throw new Exception('MySQL delete query error: ' . mysqli_error($this->conn));
 		}
 
