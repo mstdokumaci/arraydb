95b2515881ff859489da18ee3ecebfc421eb3ce9
mustafa <mustafa@foraa.com>
2012-05-18 11:32:18 +0300

    bugfix

--- sample/social/list.php
+++ sample/social/list.php
@@ -50,7 +50,7 @@
 	echo '<h1>Top 10 Users with Most Posts</h1>' . "\n";
 	echo '<ul>' . "\n";
 	foreach ($adb->id_list('user', false, 'post DESC', 10) as $id) {
-		$user=$adb->load($id);
+		$user=$adb->load('user', $id);
 		echo '<li>' . $user['name'] . '(' . count($user['post']) . ')</li>' . "\n";
 	}
 	echo '</ul>\' . "\n';
