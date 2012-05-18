055238a46d7e867239d7d2a0b4eebf4e2d4aac46
mustafa <mustafa@foraa.com>
2012-05-18 11:47:48 +0300

    fix

--- sample/social/list.php
+++ sample/social/list.php
@@ -51,7 +51,7 @@
 	echo '<ul>' . "\n";
 	foreach ($adb->id_list('user', false, 'post DESC', 10) as $id) {
 		$user=$adb->load('user', $id);
-		echo '<li>' . $user['name'] . '(' . count($user['post']) . ')</li>' . "\n";
+		echo '<li>' . $user['name'] . ' (' . count($user['post']) . ')</li>' . "\n";
 	}
 	echo '</ul>' . "\n";
 
@@ -59,7 +59,7 @@
 	echo '<ul>' . "\n";
 	foreach ($adb->id_list('post', false, 'liker DESC', 10) as $id) {
 		$post=$adb->load('post', $id);
-		echo '<li>' . $post['text'] . '(' . count($post['liker']) . ')</li>' . "\n";
+		echo '<li>' . $post['text'] . ' (' . count($post['liker']) . ')</li>' . "\n";
 	}
 	echo '</ul>' . "\n";
 
@@ -67,13 +67,13 @@
 	echo '<ul>' . "\n";
 	foreach ($adb->id_list('post', false, 'comment DESC', 10) as $id) {
 		$post=$adb->load('post', $id);
-		echo '<li>' . $post['text'] . '(' . count($post['comment']) . ')</li>' . "\n";
+		echo '<li>' . $post['text'] . ' (' . count($post['comment']) . ')</li>' . "\n";
 	}
 	echo '</ul>' . "\n";
 	echo '<h1>Top 10 Comments with Most Likes</h1>' . "\n";
 	echo '<ul>' . "\n";
 	foreach ($adb->id_list('comment', false, 'liker DESC', 10) as $id) {
 		$comment=$adb->load('comment', $id);
-		echo '<li>' . $comment['text'] . '(' . count($comment['liker']) . ')</li>' . "\n";
+		echo '<li>' . $comment['text'] . ' (' . count($comment['liker']) . ')</li>' . "\n";
 	}
 	echo '</ul>' . "\n";
