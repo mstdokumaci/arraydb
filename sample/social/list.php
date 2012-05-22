64c92e947b864a8c0932eedce6fdbd0fe705ca9e
mustafa <mustafa@foraa.com>
2012-05-22 11:05:57 +0300

    top 5

--- sample/social/list.php
+++ sample/social/list.php
@@ -47,32 +47,32 @@
 		echo '</ul>' . "\n";
 	}
 
-	echo '<h1>Top 10 Users with Most Posts</h1>' . "\n";
+	echo '<h1>Top 5 Users with Most Posts</h1>' . "\n";
 	echo '<ul>' . "\n";
-	foreach ($adb->id_list('user', false, 'post DESC', 10) as $id) {
+	foreach ($adb->id_list('user', false, 'post DESC', 5) as $id) {
 		$user=$adb->load('user', $id);
 		echo '<li>' . $user['name'] . ' (' . count($user['post']) . ')</li>' . "\n";
 	}
 	echo '</ul>' . "\n";
 
-	echo '<h1>Top 10 Posts with Most Likes</h1>' . "\n";
+	echo '<h1>Top 5 Posts with Most Likes</h1>' . "\n";
 	echo '<ul>' . "\n";
-	foreach ($adb->id_list('post', false, 'liker DESC', 10) as $id) {
+	foreach ($adb->id_list('post', false, 'liker DESC', 5) as $id) {
 		$post=$adb->load('post', $id);
 		echo '<li>' . $post['text'] . ' (' . count($post['liker']) . ')</li>' . "\n";
 	}
 	echo '</ul>' . "\n";
 
-	echo '<h1>Top 10 Posts with Most Comments</h1>' . "\n";
+	echo '<h1>Top 5 Posts with Most Comments</h1>' . "\n";
 	echo '<ul>' . "\n";
-	foreach ($adb->id_list('post', false, 'comment DESC', 10) as $id) {
+	foreach ($adb->id_list('post', false, 'comment DESC', 5) as $id) {
 		$post=$adb->load('post', $id);
 		echo '<li>' . $post['text'] . ' (' . count($post['comment']) . ')</li>' . "\n";
 	}
 	echo '</ul>' . "\n";
-	echo '<h1>Top 10 Comments with Most Likes</h1>' . "\n";
+	echo '<h1>Top 5 Comments with Most Likes</h1>' . "\n";
 	echo '<ul>' . "\n";
-	foreach ($adb->id_list('comment', false, 'liker DESC', 10) as $id) {
+	foreach ($adb->id_list('comment', false, 'liker DESC', 5) as $id) {
 		$comment=$adb->load('comment', $id);
 		echo '<li>' . $comment['text'] . ' (' . count($comment['liker']) . ')</li>' . "\n";
 	}
